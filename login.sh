682b21231065f00039a67e4c74f2b4fd2a342c0d
whiskerz007 <2713522+whiskerz007@users.noreply.github.com>
2019-03-29 15:52:56 +0100

    Fixed #3 (#5)
    * Updated login.sh to match changes to tuya-convert
    * Check for updated tuya-convert

--- login.sh
+++ login.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
-trap "{ echo 'Terminate'; ./stop_flash.sh; exit 1; }" SIGINT SIGTERM
+trap "{ echo -e '\nTerminate'; ./stop_flash.sh; exit 1; }" SIGINT SIGTERM
 
 cd /root/tuya-convert/
+mapfile -t REF < <(git show-ref master | sed -e 's/^\(.*\)\(\s.*$\)/\1/')
+if [ "${REF[0]}" != "${REF[1]}" ]; then
+  echo -e "\n\n"\
+        "There is a \e[1mnew version\e[0m of '\e[100;93mtuya-convert\e[39;49m'. Consider running the\n"\
+        "script found at the following URL to ensure best possible outcome.\n\n"\
+        "https://github.com/whiskerz007/proxmox_tuya-convert_container\n"
+  read -n 1 -p "--Press any key to continue--"
+fi
 ./start_flash.sh
+echo "tuya-convert exited with code:$?"
 function menu1 (){
 while true; do
   echo -e "\n\n\n\nHere are you options for flashing your device\n\n" \
@@ -26,16 +35,16 @@ function menu2 () {
     mapfile -t LINKS < <(find /root/tuya-convert/files/ -type l -exec readlink {} \;)
     for link in "${LINKS[@]}"; do
       for i in "${!FILES[@]}"; do
-        if [[ ${FILES[i]} = "$link" ]]; then
+        if [[ ${FILES[i]} = "$link" ]] || [[ ${FILES[i]} = "upgrade.bin" ]] || [[ ${FILES[i]} = "user2.bin" ]] ; then
           unset 'FILES[i]'
         fi
       done
     done
     FILES=("${FILES[@]}")
     echo -e "\n\nHere is the list of third-party firmwares\n\n" \
-      "    1) Sonoff-tasmota"
-    for file in "${!FILES[@]}"; do
-      echo -e "     $((file+2))) ${FILES[$file]}"
+      "    1) Sonoff-tasmota (packaged with tuya-convert)"
+    for i in "${!FILES[@]}"; do
+      echo -e "     $((i+2))) ${FILES[$i]}"
     done
     echo -e "\n     b) Go back to previous menu\n"
     if [ ${#FILES[@]} -le 8 ]; then
@@ -58,32 +67,33 @@ function menu2 () {
   done
 }
 menu1
+curl_cmd="curl --fail -m 2"
 case $RESPONSE in
   1)
-    curl -m 2 http://10.42.42.42/undo
+    $curl_cmd http://10.42.42.42/undo
     ;;
   2)
-    curl -m 2 http://10.42.42.42/flash2
+    $curl_cmd http://10.42.42.42/flash2
     ;;
   3)
-    curl -m 2 http://10.42.42.42/flash3
+    $curl_cmd http://10.42.42.42/flash3
     ;;
   4)
     echo -e "\nUsing $FIRMWARE firmware..."
-    curl -m 2 http://10.42.42.42/flashURL?url=http://10.42.42.1/files/$FIRMWARE
+    $curl_cmd http://10.42.42.42/flash3?url=http://10.42.42.1/files/$FIRMWARE
     ;;
 esac
 RESULT=$?
 if [ $RESULT -ne 0 ]; then
   echo -e "\nWARNING: An error occured when trying to flash device. Dropping to shell...\n"
   /bin/bash
-  exit 1
-fi
-if [ $RESPONSE -eq 1 ]; then
-  SLEEP=2
 else
-  SLEEP=75
+  if [ $RESPONSE -eq 1 ]; then
+    SLEEP=2
+  else
+    SLEEP=75
+  fi
+  echo -e "\n\nWaiting for flash to complete.\nSleeping for $SLEEP seconds...\n"
+  sleep $SLEEP
 fi
-echo -e "\n\nWaiting for flash to complete.\nSleeping for $SLEEP seconds...\n"
-sleep $SLEEP
 ./stop_flash.sh
