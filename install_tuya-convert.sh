cb7b6f90db65a3b5c446248debf554179a951eb5
SirRedZ <44956412+SirRedZ@users.noreply.github.com>
2019-09-18 22:42:39 -0400

    Update install_tuya-convert.sh

--- install_tuya-convert.sh
+++ install_tuya-convert.sh
@@ -26,7 +26,7 @@ locale-gen
 apt update
 apt upgrade -y
 apt install -y git curl net-tools samba
-git clone https://github.com/M4dmartig4n/tuya-convert/tree/master
+git clone https://github.com/M4dmartig4n/tuya-convert
 find tuya-convert -name \*.sh -exec sed -i -e "s/sudo -E//" -e "s/sudo //" {} \;
 cd tuya-convert
 ./install_prereq.sh
