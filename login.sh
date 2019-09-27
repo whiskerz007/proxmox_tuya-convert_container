e4a41cd528807185a09d75c1ccc22a307939439a
SirRedZ <44956412+SirRedZ@users.noreply.github.com>
2019-09-27 00:41:54 -0400

    Update login.sh

--- login.sh
+++ login.sh
@@ -9,7 +9,7 @@ if [ "${REF[0]}" != "${REF[1]}" ]; then
   echo -e "\n\n"\
         "There is a \e[1mnew version\e[0m of '\e[100;93mtuya-convert\e[39;49m'. Consider running the\n"\
         "script found at the following URL to ensure best possible outcome.\n\n"\
-        "https://github.com/whiskerz007/proxmox_tuya-convert_container\n"
+        "https://github.com/SirRedZ/ProxMox-Tuya-Convert-2.0-Container\n"
   read -n 1 -p "--Press any key to continue--"
 fi
 ./start_flash.sh
