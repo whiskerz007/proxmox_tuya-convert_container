004f0b40ba7dfc26ff280d2c26e0912a54111528
Tollbringer <44956412+SirRedZ@users.noreply.github.com>
2019-09-29 09:23:11 -0400

    Update install_tuya-convert.sh

--- install_tuya-convert.sh
+++ install_tuya-convert.sh
@@ -34,6 +34,11 @@ systemctl disable mosquitto
 echo "Setting $WLAN interface for tuya-convert ..."
 sed -i "s/^\(WLAN=\)\(.*\)/\1$WLAN/" config.txt
 
+# get Digiblurs bin file WiFiman
+cd /tuya-convert/files
+wget https://raw.github.com/digiblur/Sonoff-Tasmota/development/generic/tasmota-6.6.0.7-generic-wifiman-2.3.0.bin
+mv tasmota-6.6.0.7-generic-wifiman-2.3.0.bin thirdparty.bin
+
 # Customize OS
 cat <<EOL >> /etc/samba/smb.conf
 [tuya-convert]
